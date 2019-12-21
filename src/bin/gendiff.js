c9ff2a13a4e466c7d05c2d968da7a343f3476742
Alex <blinovalexanders@gmail.com>
2019-12-21 23:50:27 +0300

    add type option and arguments to gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,4 +5,12 @@ const program = commander;
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<pathToFirstFile> <pathToSecondFile>')
+  .action((pathToFirstFile, pathToSecondFile) => {
+    const path1 = pathToFirstFile;
+    const path2 = pathToSecondFile;
+    console.log(path1, path2);
+  });
+
+program.parse(process.argv);
